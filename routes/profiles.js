44c676fb37b563ed650121ecf1619c056778f3ac
Andrew Morrison <morrison.andrew422@gmail.com>
2023-04-21 15:23:48 -0400

    add isLoggedIn and ejs

--- routes/profiles.js
+++ routes/profiles.js
@@ -1,10 +1,11 @@
 import { Router } from 'express'
 import * as profilesCtrl from '../controllers/profiles.js'
+import { isLoggedIn } from '../middleware/middleware.js'
 
 const router = Router()
 
 // GET localhost:3000/profiles
-router.get('/', profilesCtrl.index)
+router.get('/', isLoggedIn, profilesCtrl.index)
 
 export {
   router
