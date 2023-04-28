6d4849dfbd6a42b0f81e5881ff23d1793ea3a216
Andrew Morrison <morrison.andrew422@gmail.com>
2023-04-28 10:53:58 -0400

    fix comments

--- models/record.js
+++ models/record.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose'
 const Schema = mongoose.Schema
 
 const commentSchema = new Schema({
-  body: { type: String, required: true, maxLength: 60 },
+  body: { type: String, required: true, maxLength: 40 },
   commenter: { type: Schema.Types.ObjectId, ref: "Profile"},
 })
 
